786c8ff13bcfb174692f75fa9c48982150f0a62b
FeelyChau <feely@outlook.com>
2020-06-14 22:25:06 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+# coding:utf-8
+
+from setuptools import setup
+# or
+# from distutils.core import setup  
+
+setup(
+    name='cyclegan',     # 包名字
+    version='1.0',   # 包版本
+    description='This is a test of the setup',   # 简单描述
+    author='feely',  # 作者
+    author_email='pipcook@alibaba-inc.com',  # 作者邮箱
+    url='https://github.com/alibaba/pipcook',      # 包的主页
+    packages=['demo'],                 # 包
+)
