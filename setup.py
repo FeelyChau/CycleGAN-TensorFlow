075f3c3f51bebe4aeca0634dcd62d7071a5c028f
FeelyChau <feely@outlook.com>
2020-06-14 23:27:18 +0800

    fix

--- setup.py
+++ setup.py
@@ -11,5 +11,5 @@ setup(
     author='feely',  # 作者
     author_email='pipcook@alibaba-inc.com',  # 作者邮箱
     url='https://github.com/alibaba/pipcook',      # 包的主页
-    packages=['demo'],                 # 包
+    packages=[],                 # 包
 )
